028ed39f30985a28712a58d9b23ce190682fdb72
Cruz Aguilar Montserrat <cruzam2018@dci25-164.ugto.mx>
2018-08-08 09:37:28 -0500

    cree mi primer program cree mi primer programaa

---
+++ semana1/programa_hola.c
@@ -0,0 +1,19 @@
+/*
+ Esto es un comentario. Me sirve para escribir información relevante del programa que estoy haciendo. Por  ejemplo:
+ Este programa imprime la típica frase “hola mundo a la pantalla”
+ Creado el 06/08/2018 por …
+*/
+
+/*creado el 25 de Enero de 2016 por alma gonzales*/
+#include<stdio.h>
+int main ()
+{
+	printf ("Hola Alma \n");
+	printf ("Este es el primer programa del curso \n");
+
+
+}
+
+/*Termine de escribir el programa*/
+
+
