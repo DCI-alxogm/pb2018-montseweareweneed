56834fba95e6ec6758521fdf7f0f357a1f2d989e
Cruz Aguilar Montserrat <cruzam2018@dci25-164.ugto.mx>
2018-09-03 09:53:34 -0500

    Ejemplos sin alterar

---
+++ semana5/ejem2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	float exp_;
+	int n=10,j;
+
+	for(j=0;j<n;j++){
+		exp_=exp(j);
+		printf("%i \t %f\n",j,exp_);
+	}
+	return 0;
+
+}
+
--- semana5/ejemfor.c
+++ semana5/ejemfor.c
@@ -2,5 +2,21 @@
 int main()
 {
 	float temp_c, temp_k;
-	float Temp_c=100, final=200,delta;
-	int
+	float inicial,final,delta;
+	int n=10;
+	
+	printf("Dame una temperatura en grados celsius:\n");
+	scanf("%f", &inicial), 
+
+	temp_c=inicial;
+
+	delta=(final-temp_c)/n;
+	for(i=0;i<n;i++){
+		temp_k=temp_c+273.15;
+		printf("%f %f\n",temp_c,temp_k);
+		temp_c=temp_c+delta;		//	temp_c+=delta;
+
+	}
+	return 0;
+}
+
