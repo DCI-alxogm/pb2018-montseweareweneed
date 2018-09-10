00b6bf98a4d837441eeeaedb9f66d0985dfeb22d
Cruz Aguilar Montserrat <cruzam2018@dci25-164.ugto.mx>
2018-09-10 09:27:01 -0500

    Actualizar

---
+++ semana6/ejem1.c
@@ -0,0 +1,19 @@
+/*Creado el 10 de septiembre por Montserrat Cruz Aguilar*/
+#include <stdio.h>
+
+int main ()
+{
+	int i,n=10;
+	float numeros[n];
+
+
+	for(i=0;i<n;i++){
+		numeros[i]=0; //asigna 0 a cada elemento del arreglo.
+	}
+	printf("%f\n",numeros[i]);
+
+
+
+return 0;
+}
+
